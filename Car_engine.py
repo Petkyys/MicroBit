1c9eb98b6f1a02b0861ed5940ed88db9fe261fcb
Daniel Peterka <123973221+Petkyys@users.noreply.github.com>
2025-01-26 18:45:47 +0100

    Update and rename Car_engine to Car_engine.py

---
+++ Car_engine.py
@@ -0,0 +1,65 @@
+
+direction = 10
+steering = 0
+
+radio.set_group(1)
+
+'''
+Radio Signals:
+  -1  - Backwards
+  0   - Straight
+  1   - Forwards
+  2   - Left
+  3   - Right
+  10  - Parking Brake ON
+'''
+
+basic.show_leds("""
+. . . . .
+. . . . .
+# # # # #
+. . . . .
+. . . . .
+""")
+
+
+# Vehicle control unit
+def control_vehicle():
+    global direction, steering
+
+    # Movement control
+    if direction == 1:            # Forwards
+        if steering == 2:         # Turn left
+            RingbitCar.turnleft()
+        elif steering == 3:       # Turn right
+            RingbitCar.turnright()
+        else:
+            RingbitCar.forward()  # Forwards
+
+    elif direction == -1:         # Backwards
+        if steering == 2:         # Turn left
+            RingbitCar.turnleft()
+        elif steering == 3:       # Turn right
+            RingbitCar.turnright()
+        else:
+            RingbitCar.back()     # Backwards
+
+    elif direction == 10:
+        RingbitCar.brake()        # Stop
+
+
+# Decode radio input
+def on_received_number(receivedNumber):
+    global direction, steering
+
+    if receivedNumber in [-1, 10, 1]:
+        if direction != receivedNumber:
+            direction = receivedNumber
+            control_vehicle()
+
+    elif receivedNumber in [0, 2, 3]:
+        if steering != receivedNumber:
+            steering = receivedNumber
+            control_vehicle()
+
+radio.on_received_number(on_received_number)
